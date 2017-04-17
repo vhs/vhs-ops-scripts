c8727b0489c7071b48932638fc34a75a3ccdc99f
Ty Eggen <tycho.eggen@gmail.com>
2017-04-17 01:34:51 -0700

    Influxdb upgrade

--- influxdb/create.sh
+++ influxdb/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -d --name=influxdb --restart=always --volume=$DIR/data:/data --volume=$DIR/config:/config -p 8083:8083 -p 8086:8086 tutum/influxdb
+docker run -d --name=influxdb --restart=always --volume=$DIR/data:/var/lib/influxdb --volume=$DIR/config:/etc/influxdb -p 8083:8083 -p 8086:8086 influxdb
