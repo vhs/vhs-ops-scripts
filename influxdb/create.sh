60e95389b388bf4aa656614d2159a2e691bf7643
Ty Eggen <tycho.eggen@gmail.com>
2017-04-19 01:45:09 -0700

    Influxdb: added forced kill for create.sh and maintenance.sh

--- influxdb/create.sh
+++ influxdb/create.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker stop influxdb
+docker rm influxdb
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -d --name=influxdb --restart=always --volume=$DIR/data:/var/lib/influxdb --volume=$DIR/config:/etc/influxdb -p 8083:8083 -p 8086:8086 influxdb
--- influxdb/maintenance.sh
+++ influxdb/maintenance.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker stop influxdb
+docker rm influxdb
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -it --name=influxdb --volume=$DIR/backups:/backups --volume=$DIR/data:/var/lib/influxdb --volume=$DIR/config:/etc/influxdb -p 18083:18083 -p 18086:18086 --entrypoint=/bin/bash influxdb -s
