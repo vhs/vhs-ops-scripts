46a6e144ba3a41b5b89e531409bfbf04ded74558
Ty Eggen <tycho.eggen@gmail.com>
2017-04-20 16:12:03 -0700

    Modularized config

--- grafana/create.sh
+++ grafana/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -d --name=grafana --restart=always -p 3000:3000 -v $DIR/data:/var/lib/grafana grafana/grafana
+docker run -d --name=grafana --restart=always -p 3000:3000 -v $DIR/config:/etc/grafana -v $DIR/data:/var/lib/grafana grafana/grafana
