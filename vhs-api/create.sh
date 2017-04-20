f27e05022e8d379338c1c83b1698779a8679e851
Ty Eggen <tycho.eggen@gmail.com>
2017-04-20 16:19:45 -0700

    Forced pull, stop and removal of vhs-api in create.sh

--- vhs-api/create.sh
+++ vhs-api/create.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker stop vhs-api
+docker rm vhs-api
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -d --name=vhs-api --env-file ${DIR}/config.list --restart=always --link influxdb vanhack/api
 docker exec -i vhs-nginx /usr/sbin/_reload_nginx_config.sh
