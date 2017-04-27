54ed4949b2d0ec95be0b411779ed7c2dc4d7f55f
Ty Eggen <tycho.eggen@gmail.com>
2017-04-27 12:18:24 -0700

    grafana - forced pull, stop and container restart

--- grafana/create.sh
+++ grafana/create.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+docker pull grafana/grafana
+docker stop grafana
+docker rm grafana
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -d --name=grafana --restart=always -p 3000:3000 -v $DIR/config:/etc/grafana -v $DIR/data:/var/lib/grafana grafana/grafana
