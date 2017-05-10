5854944767392af5fd0dccfb7aa023a183c2550c
Ty Eggen <tycho.eggen@gmail.com>
2017-05-09 21:44:51 -0700

    vhs-api configuration directory

--- vhs-api/create.sh
+++ vhs-api/create.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+BOOTDIR=`pwd`
+
 docker pull vanhack/api
 
 docker stop vhs-api
 docker rm vhs-api
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -d --name=vhs-api --env-file ${DIR}/config.list --restart=always --link influxdb vanhack/api
+docker run -d --name=vhs-api --env-file ${DIR}/config.list -v $BOOTDIR/etc:/usr/src/app/etc --restart=always --link influxdb vanhack/api
 docker exec -i vhs-nginx /usr/sbin/_reload_nginx_config.sh
