89ed8a3117374db40b354e368546eadcb40de6de
Ty Eggen <tycho.eggen@gmail.com>
2017-11-23 19:56:50 -0800

    collectd

--- influxdb/create.sh
+++ influxdb/create.sh
@@ -4,4 +4,4 @@ docker stop influxdb
 docker rm influxdb
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -d --name=influxdb --restart=always --volume=$DIR/data:/var/lib/influxdb --volume=$DIR/config:/etc/influxdb -p 8083:8083 -p 8086:8086 influxdb
+docker run -d --name=influxdb --restart=always --volume=$DIR/data:/var/lib/influxdb --volume=$DIR/config:/etc/influxdb -p 8083:8083 -p 8086:8086 -p 127.0.0.1:25826:25826 influxdb
